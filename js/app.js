25eb821992d5e48cdfef2f2994bfc2234fddf103
mathuramg <mmg9291@gmail.com>
2018-01-23 11:47:21 -0500

    clean up code

--- js/app.js
+++ js/app.js
@@ -9,34 +9,26 @@ window.addEventListener( 'resize', onWindowResize, false );
 var level = 0;
 function animate() {
   requestAnimationFrame( animate );
+
+	//this is to create random animation in case the volume is muted by the user
 	if(player.volume.value <= -100) {
 		level = 0.9*level + 0.1*(2*Math.random());
 	} else {
 		level = meter.getLevel();
 		level = Tone.dbToGain(level);
 	}
-	deltaTime = clock.getDelta();
-	animateParticles(particleSystem_main,level*2);
-	animateParticles(particleSystem_side1,level*2);
-	animateParticles(particleSystem_side2,level*2);
-	animateParticles(particleSystem_side3,level*2);
-	animateParticles(particleSystem_side4,level*2);
-	animateParticles(particleSystem_small1,level*2);
-	animateParticles(particleSystem_small2,level*2);
-	animateParticles(particleSystem_small3,level*2);
-	animateParticles(particleSystem_small4,level*2);
+	particleSystems.forEach(function(ps){
+		animateParticles(ps.system,level*2);
+	})
   renderer.render(scene, camera);
 
 };
 
 
 function onWindowResize(){
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function toggleSound() {
