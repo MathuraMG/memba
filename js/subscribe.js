e13ae66e561c28aa8fa9862365582beee73ca2f7
mathuramg <mmg9291@gmail.com>
2018-05-20 16:31:07 -0400

    add thank you styling

--- js/subscribe.js
+++ js/subscribe.js
@@ -4,6 +4,6 @@ $( document ).ready(function() {
   $('#gform').on('submit', function(e) {
     console.log("hi hi hello hoomin");
     // $('.contact__form-input').val("")
-    $('.contact__text').html("Thank you for subscribing to Memba!")
+    $('.contact__text').html("<span style='color:#ddd; font-size: 0.85rem;'>Thank you for subscribing to Memba!<span>")
   });
 });
